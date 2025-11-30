e27d3a81c4f70b76de9d688af3770243376eab2e
SofiiA <menesescorvalansofia@gmail.com>
2025-11-29 22:08:04 -0300

    Update delivery.py

--- delivery.py
+++ delivery.py
@@ -1,8 +1,21 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+        import streamlit as st
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        
+        st.title("Restaurantes más populares por zonas")
+        df = pd.read_csv('Delivery.csv')  #Carga datos del df
+        st.sidebar.header("Opciones de Diseño")
+        color_elegido = st.sidebar.color_picker("Elige un color para las barras", "#00f900") #Permite al usuario elegir colores
+        datos_grafico = df["restaurant_zone"].value_counts() #Ordena de mayor a menor
 
-# Carga el archivo CSV "database_titanic.csv" en un DataFrame de pandas.
-df = pd.read_csv("Delivery.csv")
+       
+        fig6, ax6 = plt.subplots(figsize=(6, 4))
+        ax.bar(datos_grafico.index, datos_grafico.values, color=color_elegido) 
+        ax.set_title("Cantidad de Pedidos por Zona")
+        ax.set_xlabel("Zona del Restaurante")
+        ax.set_ylabel("Total de Pedidos")
+        st.pyplot(fig6, use_container_width=False)
 
-st.write("Página de Delivery")
+        
+        st.write("Detalle de los datos:", datos_grafico) #Números exactos
+                
